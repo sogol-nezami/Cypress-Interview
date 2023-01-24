6441b5f9238a76e5a2d046b10ddd3c400f855a8b
Sogol <sogol.nezami@divar.ir>
2023-01-24 17:30:38 +0330

    remove useless comments

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,8 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-// import './commands';
 before(() => {
   cy.openUrl()
 })
